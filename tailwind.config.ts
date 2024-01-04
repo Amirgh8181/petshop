741c1d861e2366e57dada7e623e33f9bde9ed577
AmirGh <amir.962@yahoo.com>
2024-01-04 15:05:16 +0330

    start project petShop

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,15 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        jost: ['var(--font-jost)'],
       },
     },
   },
